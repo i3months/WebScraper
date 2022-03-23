3ccc7d8eaeb6f155b3d7f0547bdeb7e739d9cc79
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-03-23 23:20:57 +0900

    update

---
+++ WebScrapper/main.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("div", {"class" : "pagination"})
+
+links = pagination.find_all('a')
+pages = []
+
+for link in links:
+  pages.append(link.find("span").string)
+pages = pages[0:-1]
+print(pages)
+
+  
